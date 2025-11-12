7111acfda44e7bb36bee9775f603452d26598176
egorzam21 <147714238+egorzam21@users.noreply.github.com>
2025-11-12 20:51:11 +0300

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,26 @@
+<?php
+// send_email.php
+
+// Получаем данные из запроса
+$input = json_decode(file_get_contents('php://input'), true);
+
+// Настройки email
+$to = "info@kopilka.ru"; // Замените на нужный email
+$subject = "Обратная связь с сайта Копилка: " . $input['topic'];
+$message = "
+Имя: " . $input['name'] . "
+Email: " . $input['email'] . "
+Тема: " . $input['topic'] . "
+Сообщение: " . $input['message'];
+
+$headers = "From: " . $input['email'] . "\r\n" .
+           "Reply-To: " . $input['email'] . "\r\n" .
+           "X-Mailer: PHP/" . phpversion();
+
+// Отправляем email
+$success = mail($to, $subject, $message, $headers);
+
+// Возвращаем ответ
+header('Content-Type: application/json');
+echo json_encode(['success' => $success]);
+?>
